42224f6f9b153b257f1a3ea90fecbfe69886dde5
Leonel <leo.villalbaa13@gmail.com>
2023-06-08 17:23:04 -0300

    Modificasiones 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { MoviesGrid } from "./components/MoviesGrid";
+// import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
 
 
 
 export function App() {
-  const Home = () => <h1>Home</h1>;
-  const Movie = () => <h1>Movie</h1>;
   const NotFound = () => <h1>404 - Not Found</h1>;
   return (
     <Router>
       <header>
-        {/* <h1 className={styles.title}>Movies</h1> */}
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/movie">Movie</Link>
+      <Link to="/"><h1 className={styles.title}>Movies</h1></Link>
+        
       </header>
       <main>
         <Routes>
-          <Route path="/movie" element = {<Movie/>} ></Route>
-          <Route path="/" element = {<Home/>}></Route>
+          <Route path="/movies/:movieId" element = {<MovieDetails/>} ></Route>
+          <Route path="/" element = {<LandingPage/>}></Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
-import movies from "./movies.json";
+// import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/discover/movie", {
+      headers:{
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmQ5M2IxMmNhMWM3MzI2NDNjNDcyOGU5MDU4MmIxYSIsInN1YiI6IjY0N2YzMWM5MGUyOWEyMmJlMWYxMjkxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RwFO0dw0k4FwNGt-SkgiSVfxDmnpGmUarzQvNGkwAJA",
+      "Content-Type": "application/json;charset=utf-8",
+      },
+    })
+      .then((result) => result.json())
+      .then((data) => {
+        setMovies(data.results);
+    });
+
+  }, []);
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
