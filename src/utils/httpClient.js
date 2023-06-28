6e740dc0048319b7f223ae472f26fadb1acca1bd
Leonel <leo.villalbaa13@gmail.com>
2023-06-28 01:02:46 -0300

    codigo final

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,6 +1,6 @@
 
-const API = "https://api.themoviedb.org/3";
-// const API_TOKEN = import.meta.env.VITE_TOKEN;
+const API = import.meta.env.VITE_API_URL;
+const API_TOKEN = import.meta.env.VITE_TOKEN;
 
 // console.log(import.meta.env.VITE_TOKEN);
 // console.log(import.meta.env.VITE_API_URL);
@@ -9,7 +9,7 @@ export function get(path){
     return fetch(API + path, {
       headers:{
         Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMmQ5M2IxMmNhMWM3MzI2NDNjNDcyOGU5MDU4MmIxYSIsInN1YiI6IjY0N2YzMWM5MGUyOWEyMmJlMWYxMjkxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RwFO0dw0k4FwNGt-SkgiSVfxDmnpGmUarzQvNGkwAJA",
+        "Bearer " + API_TOKEN,
       "Content-Type": "application/json;charset=utf-8",
       },
     })
